292a132c0de60194ec7d173efe0bd913e7c1cdc7
Emily Berger <bergeremily2@gmail.com>
2023-06-25 17:29:11 -0500

    Cleared backend and re-worked everything to work at the base level

--- Backend/index.js
+++ Backend/index.js
@@ -1,23 +1,7 @@
 import express from "express";
-import users from "./routes/users.js"
-import portfolios from "./routes/portfolios.js"
-import portfolioItems from "./routes/portfolioItems.js"
-import profiles from "./routes/profiles.js"
-import payments from "./routes/payments.js"
-import commissions from "./routes/commissions.js"
-import posts from "./routes/posts.js"
-import postLikes from "./routes/postLikes.js"
-import comments from "./routes/comments.js"
-import followings from "./routes/followings.js"
-import professionalArtistInfos from "./routes/professionalArtistInfos.js"
-import emails from "./routes/emails.js"
-import login from "./routes/auth.js"
-import recruiterInfos from "./routes/recruiterInfos.js"
-import tokenRoutes from "./routes/tokenroutes.js"
 
 import cors from "cors";
 import helmet from "helmet";
-import tags from "./routes/tags.js"
 
 const app = express();
 
@@ -29,25 +13,6 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Welcome to the Comm API!");
 });
-
-app.use(users);
-app.use(portfolios);
-app.use(portfolioItems);
-app.use(profiles);
-app.use(payments);
-app.use(commissions);
-app.use(posts);
-app.use(postLikes);
-app.use(comments);
-app.use(followings);
-app.use(professionalArtistInfos);
-app.use(emails);
-app.use(login);
-app.use(recruiterInfos);
-app.use(tags);
-app.use(tokenRoutes);
-app.use(followings);
-app.use(comments);
 
 app.use((err, req, res, next) => {
   if (err) {
